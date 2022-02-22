181b02ce050985e32a1aaa307d108050e3f2bb9a
CodeNinja04 <hemendrasharma2019@gmail.com>
2022-02-22 20:19:02 +0530

    contract.py updated

--- backend/router/worker.py
+++ backend/router/worker.py
@@ -5,20 +5,19 @@ from pydantic import BaseModel
 
 class Worker(BaseModel):
     name: str
-  
-    
-    
+
+
 router = APIRouter(
     prefix='/worker',
     tags=['worker']
 )
 
 
-
 @router.get('/')
 def index():
     return getWorkersList()
 
+
 @router.post('/')
-def index(worker:Worker):
+def index(worker: Worker):
     return setWorker(worker.name)
